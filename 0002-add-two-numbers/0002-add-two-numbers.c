7da625ff583cf7046f50612f098c7abb1c36b624
habe <hideumi613.inc@gmail.com>
2026-03-07 13:02:20 +0900

    Time: 1 ms (49.43%), Space: 13.7 MB (9.03%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.c
+++ 0002-add-two-numbers/0002-add-two-numbers.c
@@ -5,7 +5,7 @@
  *     struct ListNode *next;
  * };
  */
-
+// list_size: リストの長さを取得する
 int list_size(struct ListNode *list)
 {
     int len;
@@ -19,6 +19,9 @@ int list_size(struct ListNode *list)
     return (len);
 }
 
+// リストのvalを配列にする
+// 配列の後ろから、リストの先頭valを入れていく
+// 2->3->4 の時は [4][3][2]にして返す
 int *array(struct ListNode *list, int l_size)
 {
     int *l_re;
@@ -34,6 +37,48 @@ int *array(struct ListNode *list, int l_size)
     return (l_re);
 }
 
+// 配列の後ろからtmpに足していく
+// result->valには　tmp % 10を入れて1の位を格納
+// tmp /= 10 は　１の位を足した時に繰り上がりがある時、繰り上がり分だけにする
+// l1, l2のサイズがまだ残っている || 繰り上がりがある場合はlist->nextをmallocし確保
+struct ListNode* list_sum(int *l1, int l1_s, int *l2, int l2_s)
+{
+	int	tmp;
+	struct ListNode	*sum;
+    struct ListNode *head;
+
+	tmp = 0;
+	sum = malloc(sizeof(struct ListNode));
+	if (sum == NULL)
+		return (NULL);
+	sum->val = 0;
+	sum->next = NULL;
+    head = sum;
+	while (1)
+    {
+        l1_s--;
+        l2_s--;
+        if (l1_s >= 0)
+            tmp += l1[l1_s];
+        if (l2_s >= 0)
+            tmp += l2[l2_s];
+        sum->val = tmp % 10;
+        tmp /= 10;
+        if (l1_s > 0 || l2_s > 0 || tmp > 0)
+        {
+            sum->next = malloc(sizeof(struct ListNode));
+            if (sum->next == NULL)
+                return (NULL);
+            sum = sum->next;
+            sum->val = 0;
+            sum->next = NULL;
+        }
+        else
+            break ;
+    }
+	return (head);
+}
+
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
 {
     int l1_size = list_size(l1);
@@ -41,37 +86,12 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
     int l2_size = list_size(l2);
     int *l2_a = array(l2, l2_size);
     int tmp;
-    struct ListNode *head;
     struct ListNode *result;
 
-    result = malloc(sizeof(struct ListNode));
+    result = list_sum(l1_a, l1_size, l2_a, l2_size);
     if (result == NULL)
-        return (NULL);
-    result->val = 0;
-    result->next = NULL;
-    head = result;
-    tmp = 0;
-    while (1)
-    {
-        l1_size--;
-        l2_size--;
-        if (l1_size >= 0)
-            tmp += l1_a[l1_size];
-        if (l2_size >= 0)
-            tmp += l2_a[l2_size];
-        result->val = tmp % 10;
-        tmp /= 10;
-        if (l1_size > 0 || l2_size > 0 || tmp > 0)
-        {
-            result->next = malloc(sizeof(struct ListNode));
-            result = result->next;
-            result->val = 0;
-            result->next = NULL;
-        }
-        else
-            break ;
-    }
+		return (NULL);
     free(l1_a);
     free(l2_a);
-    return (head);
+    return (result);
 }
