56ed8dbb2411e60921ac757ee36bd48bbfdf0a16
habe <hideumi613.inc@gmail.com>
2026-03-17 17:29:31 +0900

    Time: 0 ms (100%), Space: 8.6 MB (51.83%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.c
@@ -0,0 +1,65 @@
+// int is_val3(char *s, char c, char b)
+// {
+//     int opened = 0, closed = 0;
+
+//     for (int i = 0; str[i]; i++)
+//     {
+//         if (str[i] == c)
+//             opened++;
+//         else if (str[i] == b)
+//         {
+//             if (opened > 0)
+//                 opened--;
+//             else
+//                 closed++;
+//         }
+//     }
+//     return (opened + closed);
+// }
+
+int is_open(char c)
+{
+    if (c == '(' || c == '{' || c == '[')
+        return (1);
+    return (0);
+}
+
+int check_pair(char open, char close)
+{
+    if (open == '(' && close == ')')
+        return (1);
+    if (open == '[' && close == ']')
+        return (1);
+    if (open == '{' && close == '}')
+        return (1);
+    return (0);
+}
+
+bool isValid(char* s) 
+{
+    int stack[strlen(s)];
+    int n;
+
+    n = 0;
+
+    if (!is_open(s[0]))
+        return (false);
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        if (is_open(s[i]) == 1)
+        {
+            stack[n] = s[i];
+            n++;
+        }
+        else if (n != 0 && check_pair(stack[n - 1], s[i]) == 1)
+        {
+            n--;
+            stack[n] = 0;
+        }
+        else 
+            return(false);
+    }
+    if (stack[0] != 0)
+        return (false);
+    return (true);
+}
